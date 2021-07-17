e6d590ca11578ff327b016f6f4246ad680df38d6
sin-sky-sora <sinskyozz@gmail.com>
2021-07-17 20:10:55 +0900

    rename template -> templates

--- myDashboard/myDashboard/settings.py
+++ myDashboard/myDashboard/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'myDashboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'template'],
+        'DIRS': [BASE_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
