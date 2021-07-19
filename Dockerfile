d7e663003d7f29157c031ef2c667cc24befd49eb
sin-sky-sora <sinskyozz@gmail.com>
2021-07-20 00:09:18 +0900

    downdetector add

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:slim
 
 # RUN apt-get update && apt-get install pipenv nodejs
 
-RUN pip install django django-3-jet django-tailwind
+RUN pip install django django-3-jet django-tailwind requests bs4
--- myDashboard/dashApp/views.py
+++ myDashboard/dashApp/views.py
@@ -1,7 +1,6 @@
-from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
-
+import re
 import requests
 from bs4 import BeautifulSoup as bs
 from bs2json import bs2json
@@ -12,8 +11,33 @@ def top(request):
 
 def get_aws_rss(request) -> JsonResponse:
     url =  request.GET["url"] if request.GET["url"] else None
-    response = requests.get(url)
-    soup = bs(response.text,"html.parser")
-    converter = bs2json()
-    data = converter.convert(soup.channel)
+    if url is None:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = bs(response.text,"html.parser")
+            converter = bs2json()
+            data = converter.convert(soup.channel)
+            data["status"] = "success"
+        else:
+            data = {"status":"error","log":"Incorrect URL","status code":response.status_code}
+    else:
+        data = {"status":"error","log":"no URL"}
+    return JsonResponse(data)
+
+def get_downdetector(request) -> JsonResponse:
+    link = "https://downdetector.jp/archive"
+    response = requests.get(link)
+    if response.status_code == 200:
+            soup = bs(response.text,"html.parser")
+            data = {"status":"success","table":[]}
+            for item in soup.select("#page table tr.border"):
+                jdata = {}
+                item_data = item.select("td")
+                jdata["name"] = re.sub("\s","",item_data[0].text)
+                jdata["link"] = item_data[0].find("a").get("href")
+                jdata["date"] = re.sub("\s","",item_data[1].text)
+                jdata["time"] = re.sub("\s","",item_data[2].text)
+                data["table"].append(jdata)
+    else:
+        data = {"status":"error","log":"Incorrect URL","status code":response.status_code}
     return JsonResponse(data)
